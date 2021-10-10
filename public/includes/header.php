688f08c6d5f277b18a3299a64262a54c9710f7c0
Peluc1 <matheussilvagoncalves18@gmail.com>
2021-10-09 23:02:54 -0300

    includes

---
+++ public/includes/header.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title><?=$page_title?></title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/projeto.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+        <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <header>
+    <nav class="menuprincipal">
+      <div class="navbar" id="navbar">
+        <div class="logo"><a href="public/index.php">MyTeams</a></div>
+        <div class="nav-links pre-ender">
+          <ul class="links">
+            <li><a href="#" class="home2">Home</a></li>
+            <li><a href="produtos.php" class="home2">Produtos</a></li>
+            <li><a href="#" class="home2">Sobre Nós</a></li>
+          </ul>
+        </div>
+
+        <div class="nav-links ender">
+            <ul class="links">
+                <li>
+                    <i class='bx bx-user-circle'></i>
+                    <ul class="perfilsubmenu submenuprincipal">
+                        <li><a href="#">Login</li></a>
+                        <li><a href="public/perfil.php">Perfil</li></a>
+                    </ul>
+                </li>
+                <li>
+                    <i class='bx bx-cart' ></i>
+                </li>
+            </ul>
+            <div class="search-box">
+                <i class='bx bx-search' ></i>
+                <div class="input-box pesquisar">
+                    <input type="text" placeholder="O que procura?">
+                </div>
+            </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <nav class="submenu" id="submenu">
+    <div class="navbar">
+      <div class="nav-links">
+        <ul class="links">
+          <li>
+            <a href="#" class="home2">Assinaturas</a>
+            <i class='bx bx-up-arrow-alt arrow assinaturasarrow' ></i>
+            <ul class="perfilsubmenu submenuprincipal">
+                <li><a href="#">Netflix</li></a>
+                <li><a href="#">Disney+</li></a>
+              </ul>
+          </li>
+          <li>
+            <a href="#" class="home2">Skins e itens</a>
+            <i class='bx bx-up-arrow-alt arrow skinsitensarrow' ></i>
+            <ul class="perfilsubmenu submenuprincipal">
+                <li><a href="#">Netflix</li></a>
+                <li><a href="#">Disney+</li></a>
+              </ul>
+          </li>
+          <li>
+            <a href="#" class="home2">Contas</a>
+            <i class='bx bx-up-arrow-alt arrow contasarrow' ></i>
+            <ul class="perfilsubmenu submenuprincipal">
+                <li><a href="#">Netflix</li></a>
+                <li><a href="#">Disney+</li></a>
+              </ul>
+          </li>
+          <li>
+            <a href="#" class="home2">Jogos</a>
+            <i class='bx bx-up-arrow-alt arrow jogosarrow' ></i>
+            <ul class="perfilsubmenu submenuprincipal">
+                <li><a href="#">Netflix</li></a>
+                <li><a href="#">Disney+</li></a>
+              </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
